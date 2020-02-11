aad8cbff1e5f1311bf372418929ebd17f62e40bb
ranraok <46853842+ranraok@users.noreply.github.com>
2020-02-11 14:00:18 -0500

    addded click and double click events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+
+//Event Listener List
+/*
+mouseover
+keydown
+wheel
+drag/drop
+load
+focus
+resize
+scroll
+select
+//dblclick
+//click
+*/
+
+
+// click
+
+var myTitle = document.getElementsByTagName('h1')[0]
+
+myTitle.addEventListener('click', function(){
+	myTitle.style.color = 'red';
+})
+
+//doubleclick
+
+myTitle.addEventListener('dblclick', function(){
+	myTitle.style.color = 'black';
+})
+
+
+
